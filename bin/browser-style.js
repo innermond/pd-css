07ec81254c72676fe87b2ac27ab93338446b623c
innermond <innermond@yahoo.com>
2022-03-02 20:12:05 +0200

    inotifywait as bash script+css movements

--- bin/browser-style.js
+++ bin/browser-style.js
@@ -1,5 +1,9 @@
-void async function() {
+window.addEventListener(
+  'focus',
+async function() {
+  console.log('raised');
 resp = await fetch('http://localhost:3000?s=style');
 txt = await resp.text();
 document.getElementById('basestyle').textContent = txt;
-}()
+}
+ ) 
